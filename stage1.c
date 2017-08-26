805f8891b3eb94d4ec6a090556a011a8475efec6
kennyseven <kenny_seven@icloud.com>
2017-08-26 14:20:03 +1000

    Add traverse function which finds the prefix location at the tree and the print function for any words that starts with the prefix

--- stage1.c
+++ stage1.c
@@ -30,8 +30,6 @@ struct node
 
 }
 
-
-
 /* The insert function takes in a pointer to node, a pointer to a word,
 and a weight. If the node is empty create a new node. If the word is less,
 equal or greater than the current word in pNode, then recursively call the
@@ -68,20 +66,79 @@ struct node
     return pNode;
 }
 
-
-
-
+/* The function takes the pointer to a node and a pointer to an input prefix
+if the prefix is not nul nor NULL, then the loop would initiate the
+traversing of the tree to the end of the prefix char. At the end of the prefix
+char, a traverse function is used to print out all the characters that have
+the prefix */
 
 void
 find_and_traverse(struct node *pNode, char *prefix){
 
+    while( *prefix != '\0 ' && pNode != NULL){
 
+        if(prefix < pNode->data){
+            find_and_traverse(pNode->left, prefix);
+            continue;
+
+        }
+
+        if(prefix > pNode->right){
+            find_and_traverse(pNode->right, prefix);
+            continue;
+
+        }
+
+        if(prefix == pNode->equal){
+            find_and_traverse(pNode->equal, prefix+1);
+            continue;
+
+        }
+
+    }
+
+    if(pNode){
+        if(pNode->end_of_key == TRUE){
+            buffer[strlen(prefix)+1] = '\0';
+            printf("%s\n", buffer);
+
+        }
+
+        traverse(pNode, buffer, strlen(prefix));
+    }
 
     return;
 }
 
+/* This function traverses through the tree for a given prefix and a buffer
+that holds a prefix and finds if there is a word that is equivalent to that
+prefix and 1 char more at each recursion. It then prints out every different
+word that exist since it checks for the flags */
+
 void
-traverse(struct node* pNode, char *buffer, int depth)
+traverse(struct node* pNode, char *buffer, int depth){
+
+    if(!pNode) return;
+
+    traverse(pNode->left, buffer, depth);
+
+    buffer[depth] = pNode->data;
+
+    if(pNode->end_of_key == TRUE){
+        buffer[depth+1] = '\0';
+        printf("%s\n", buffer);
+
+    }
+
+    /* Depth is basically the next character of the prefix and
+    it would be going down from the tree */
+    traverse(pNode->equal, buffer, depth+1);
+
+
+    traverse(pNode->right, buffer, depth);
+
+    return;
+}
 
 
 
